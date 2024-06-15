2e5387bea7e7cc8765c7ec2bb4d151249eebbdae
hsuloong <hsuloong@outlook.com>
2024-06-15 22:57:17 +0800

    行业+龙虎榜联合分析

--- technical_analysis/industry_analysis.go
+++ technical_analysis/industry_analysis.go
@@ -89,3 +89,83 @@ func StartHotIndustryHotStockAnalysis() {
 		}
 	}
 }
+
+func StartHotIndustryLhbAnalysis() {
+	const kYearTradeDays int = 250
+	const kMaxYears int = 14
+	const kTargetYear int = 5
+	const kMinAvgLhbTimes float64 = 0.0
+
+	whole_industries := data_center.GetWholeIndustryStockItems()
+	for _, industry_item := range whole_industries {
+		industry_stock_items := data_center.GetIndexContainStockItems(industry_item)
+		industry_stock_items_len := len(industry_stock_items)
+
+		var target_lhb_times int64 = 0
+		result := ""
+		for i := 1; i <= kMaxYears; i++ {
+			var loop_lhb_times int64 = 0
+			for _, stock_item := range industry_stock_items {
+				kline_items := data_center.GetKlineItems(data_center.KlineType_Day, stock_item, 250*14)
+				kline_items_len := len(kline_items)
+
+				days_before := i * kYearTradeDays
+				if kline_items_len < days_before {
+					continue
+				}
+
+				loop_lhb_times += CalculatePeriodStockLhbTimes(stock_item, kline_items, kline_items_len-days_before, kline_items_len)
+			}
+
+			if kTargetYear == i {
+				target_lhb_times = loop_lhb_times
+			}
+
+			result += fmt.Sprintf("最近%d年 %f ", i, float64(loop_lhb_times)/float64(industry_stock_items_len*i))
+		}
+
+		if float64(target_lhb_times)/float64(industry_stock_items_len*kTargetYear) >= kMinAvgLhbTimes {
+			fmt.Printf("%s(%s) %s\n", industry_item.Name, industry_item.Symbol, result)
+		}
+	}
+}
+
+func StartHotIndustryLhbHotStockAnalysis() {
+	const kMaxDaysBefore int = 250 * 1
+	const kMaxStocks int = 5
+
+	hot_idustries := "BK20720500, BK20720600, BK20461100, BK20430300, BK20720700, BK20280600, BK20280500, BK20110200, BK20510100, BK20280300, BK20240400, BK20720400"
+
+	whole_industries := data_center.GetWholeIndustryStockItems()
+	for _, industry_item := range whole_industries {
+		if !strings.Contains(hot_idustries, industry_item.Symbol) {
+			continue
+		}
+		industry_stock_items := data_center.GetIndexContainStockItems(industry_item)
+
+		lhb_stock := make([]string, 0)
+		for _, stock_item := range industry_stock_items {
+			kline_items := data_center.GetKlineItems(data_center.KlineType_Day, stock_item, 250*14)
+			kline_items_len := len(kline_items)
+			if kline_items_len < kMaxDaysBefore {
+				continue
+			}
+
+			loop_lhb_times := CalculatePeriodStockLhbTimes(stock_item, kline_items, kline_items_len-kMaxDaysBefore, kline_items_len)
+			if loop_lhb_times > 0 {
+				lhb_stock = append(lhb_stock, fmt.Sprintf("%d_%s(%s)", loop_lhb_times, stock_item.Name, stock_item.Symbol))
+			}
+		}
+
+		sort.Slice(lhb_stock, func(i, j int) bool {
+			i_value, _ := strconv.Atoi(lhb_stock[i][0:strings.Index(lhb_stock[i], "_")])
+			j_value, _ := strconv.Atoi(lhb_stock[j][0:strings.Index(lhb_stock[j], "_")])
+			return i_value > j_value
+		})
+
+		fmt.Printf("%s: \n", industry_item.Name)
+		for i := 0; i < kMaxStocks && i < len(lhb_stock); i++ {
+			fmt.Printf("%s\n", lhb_stock[i])
+		}
+	}
+}
