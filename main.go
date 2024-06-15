5dae61262d5f25bdaf326987768f39748907355c
hsuloong <hsuloong@outlook.com>
2024-06-15 22:59:13 +0800

    main

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"time"
 
-	technic_alanalysis "github.com/hsuloong/stock_speculation/technical_analysis"
+	technical_analysis "github.com/hsuloong/stock_speculation/technical_analysis"
 )
 
 var func_name = flag.String("f", "StartBacktesting", "运行的函数")
@@ -20,9 +20,27 @@ func main() {
 
 	start := time.Now().Local().Unix()
 	if *func_name == "StartBacktesting" {
-		technic_alanalysis.StartBacktesting()
+		technical_analysis.StartBacktesting()
 	} else if *func_name == "StartSelectStock" {
-		technic_alanalysis.StartSelectStock()
+		technical_analysis.StartSelectStock()
+	} else if *func_name == "StartSelectEtf" {
+		technical_analysis.StartSelectEtf()
+	} else if *func_name == "StartLhbAnalysis" {
+		technical_analysis.StartLhbAnalysis()
+	} else if *func_name == "StartEtfLofLowAnalysis" {
+		technical_analysis.StartEtfLofLowAnalysis()
+	} else if *func_name == "StartEtfLofVolatilityAnalysis" {
+		technical_analysis.StartEtfLofVolatilityAnalysis()
+	} else if *func_name == "StartEtfLofGridTradingAnalysis" {
+		technical_analysis.StartEtfLofGridTradingAnalysis()
+	} else if *func_name == "StartHotIndustryAnalysis" {
+		technical_analysis.StartHotIndustryAnalysis()
+	} else if *func_name == "StartHotIndustryHotStockAnalysis" {
+		technical_analysis.StartHotIndustryHotStockAnalysis()
+	} else if *func_name == "StartHotIndustryLhbAnalysis" {
+		technical_analysis.StartHotIndustryLhbAnalysis()
+	} else if *func_name == "StartHotIndustryLhbHotStockAnalysis" {
+		technical_analysis.StartHotIndustryLhbHotStockAnalysis()
 	}
 	end := time.Now().Local().Unix()
 	fmt.Printf("Total Cost: %ds\n", end-start)
